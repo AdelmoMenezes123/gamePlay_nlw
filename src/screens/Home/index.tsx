761b85af759d6c2f8a807efa205ac8865ba75858
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-06-25 23:48:06 -0300

    cria component appointment

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtoAdd";
 import { CategorySelect } from "../../components/CategorySelect";
@@ -7,7 +7,16 @@ import { ListHeader } from "../../components/ListHeader";
 import { styles } from './styles';
 
 export function Home() {
-    const [category, setCategory] = useState('')
+    const [category, setCategory] = useState('');
+    const apointments = [
+        {
+            id: '1',
+            guild: { id: '1', name: 'Legendarios', icon: null, owner: true },
+            category: '1',
+            date: '26/06 as 20:00h',
+            description: 'É hoje que vamos chegar ao challenger sem uma partida da md10'
+        }]
+
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId)
     }
@@ -25,8 +34,17 @@ export function Home() {
                 />
                 <View style={styles.content} >
                     <ListHeader
-                    title="Partida agendadas"
-                    subtitle="Total 6"
+                        title="Partida agendadas"
+                        subtitle="Total 6"
+                    />
+                    <FlatList
+                        data={apointments}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <Text>
+                                {item.guild.name}
+                            </Text>
+                        )}
                     />
                 </View>
             </View>
